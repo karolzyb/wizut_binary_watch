6fec7c90a966be46c7a84df2a96d12b7b7ed6baa
Karol Zyber <karol.zyber@gmail.com>
2022-05-23 15:28:54 +0200

    Main script file updated with function to translate integers into list of binaries and function to switch on/off led lights according to current binary value of hours/minutes/sec

--- binary_watch.py
+++ binary_watch.py
@@ -1,6 +1,6 @@
 #----------------------------------------------------------------------------
 # Created By  : Karol Zyber
-# Created Date: 2022-04-27
+# Created Date: 2022-05-23
 # version ='1.0'
 # ---------------------------------------------------------------------------
 # Binary watch - python3 script
@@ -15,6 +15,53 @@ import sys
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+# lists for single binary digits of hours, minutes, seconds
+list_h_binary = [None]
+list_m_binary = [None]
+list_s_binary = [None]
+
+# lists of GPIO pins to LEDs reffering to hours, minutes, seconds
+list_h_gpio = [20, 16, 12, 25, 24]
+list_m_gpio = [26, 19, 13, 6, 5, 22]
+list_s_gpio = [21]
+
+
+def int_to_bin_list(input_integer, max_len):
+    # declaring variable for storing return output
+    output_binary_list = [None]
+    # using format() + list comprehension
+    # decimal to binary number conversion 
+    output_binary_list = [int(i) for i in list('{0:0b}'.format(input_integer))]
+    # making the array of minimal expected length (adjusting to the number of LEDs to light
+    # max_len: 5 for hours, 6 for minutes, 1 for seconds)
+    while len(output_binary_list) < max_len:
+        output_binary_list = [0] + output_binary_list
+    return output_binary_list
+
+def binary_to_led(list_b, list_g):
+    # iterate thru the list to turn on/off the LEDs
+    for i in range(len(list_g)):
+        if list_b[i] == 0:
+            # print ("i: " + str(i) + " element z binary == 0")
+            # print ("i: " + str(i) + " wartosc list_binary[i]: " + str(list_binary[i]))
+            GPIO.output(list_g[i], GPIO.LOW)
+        else:
+            # print ("i: " + str(i) + " element z binary == 1")
+            # print ("i: " + str(i) + " wartosc list_binary[i]: " + str(list_binary[i]))
+            GPIO.output(list_g[i], GPIO.HIGH)
+
 
 def main():
     print("Binary watch - default set to GMT/UTC timezone")
@@ -72,16 +119,19 @@ def main():
             display_hour = current_time.tm_hour
             display_min = current_time.tm_min
             display_sec = current_time.tm_sec
-        # wyswietlanie w formie dziesietnej
-        print(f"Decimal watch: {display_hour:02d}:{display_min:02d}:{display_sec}")
-        # wyswietlanie w formie binarnej
-        print(f"Binary watch: {display_hour:05b}:{display_min:06b}:{((display_sec)%2):b}")
-        if display_sec%2==0:
-            GPIO.output(21, GPIO.LOW)
-        else:
-            GPIO.output(21, GPIO.HIGH)
-        time.sleep(1)
+        # # wyswietlanie w formie dziesietnej
+        # print(f"Decimal watch: {display_hour:02d}:{display_min:02d}:{display_sec}")
+        # # wyswietlanie w formie binarnej
+        # print(f"Binary watch: {display_hour:05b}:{display_min:06b}:{((display_sec)%2):b}")
 
+        list_h_binary = int_to_bin_list(display_hour, 5)
+        list_m_binary = int_to_bin_list(display_min, 6)
+        list_s_binary = int_to_bin_list(display_sec%2, 1)
+        binary_to_led(list_h_binary, list_h_gpio)
+        binary_to_led(list_m_binary, list_m_gpio)
+        binary_to_led(list_s_binary, list_s_gpio)
+
+        time.sleep(1)
 
 
 try:
@@ -89,4 +139,12 @@ try:
 
 except KeyboardInterrupt:
     print("Program stopped")
+    # setting all gpios as LOW
+    for i in range(len(list_h_gpio)):
+        GPIO.output(list_h_gpio[i], GPIO.LOW)
+    for i in range(len(list_m_gpio)):
+        GPIO.output(list_m_gpio[i], GPIO.LOW)
+    for i in range(len(list_s_gpio)):
+        GPIO.output(list_s_gpio[i], GPIO.LOW)
+    # cleanup for setting gpios as input (default)
     GPIO.cleanup()
--- int_to_bin_array_led.py
+++ int_to_bin_array_led.py
@@ -8,6 +8,7 @@ GPIO.setup(19, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(6, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 
 def binary_to_led(list_b, list_g):
@@ -22,39 +23,46 @@ def binary_to_led(list_b, list_g):
             # print ("i: " + str(i) + " wartosc list_binary[i]: " + str(list_binary[i]))
             GPIO.output(list_g[i], GPIO.HIGH)
 
+def int_to_bin_list(input_integer):
+    # declaring variable for storing return output
+    output_binary_list = [None]
+    # using format() + list comprehension
+    # decimal to binary number conversion 
+    output_binary_list = [int(i) for i in list('{0:0b}'.format(input_integer))]
+    # making the array of minimal expected length (adjusting to the number of LEDs to light: 5 for hours, 6 for minutes)
+    while len(output_binary_list) < 6:
+        output_binary_list = [0] + output_binary_list
+    return output_binary_list
+
+
 
 try:
 
     # list for single binary digits
     list_binary = [None]
 
+    # list of GPIO pins to LEDs
+    list_gpio = [26, 19, 13, 6, 5, 22]
+
     # initializing number 
     # int_input = 1
     int_input = int(input("Pass only an int value: "))
 
-    # safety check for limited value of int_input
-    if int_input >24:
+    # safety check for limited value of int_input: 24 for hours, 60 for minutes
+    if int_input >60:
         GPIO.cleanup()
-        print("Program ended due to int >24")
+        print("Program ended due to int >60")
         quit()
     
     # printing original number
     print ("The original number is : " + str(int_input))
     
-    # using format() + list comprehension
-    # decimal to binary number conversion 
-    list_binary = [int(i) for i in list('{0:0b}'.format(int_input))]
-
-    # making the array of minimal expected length (adjusting to the number of LEDs to light: 5 for hours, 6 for minutes)
-    while len(list_binary) < 5:
-        list_binary = [0] + list_binary
+    # using int_to_bin_list function to transform int variable into list of binaries
+    list_binary = int_to_bin_list(int_input)
 
     # printing the single items of the array
     for x in range(len(list_binary)):
         print(("Place ") + str(x) + ": " + str(list_binary[x]))
-
-    # list of GPIO pins to LEDs
-    list_gpio = [26, 19, 13, 6, 5]
 
     # lighting up LEDs that correspond to positive binary positions
     binary_to_led(list_binary, list_gpio)
